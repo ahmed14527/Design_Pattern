43a34ddb50459e96be3fa2bd7a3c25ffe55afcd9
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-08-04 03:47:38 +0300

    singleton

---
+++ singleton.py
@@ -0,0 +1,27 @@
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class CurrencyConverter(metaclass=Singleton):
+    rates = {'USD': {'EGP': 40}, 'EGP': {'USD': 0.025}}
+
+    def convert(self, amount, from_currency, to_currency):
+        if from_currency == to_currency:
+            return amount
+        else:
+            return amount * self.rates[from_currency][to_currency]
+
+if __name__ == "__main__":
+    converter = CurrencyConverter()
+
+    amount_in_usd = 200
+    amount_in_egp = converter.convert(amount_in_usd, 'USD', 'EGP')
+    print(f"{amount_in_usd} USD is equivalent to {amount_in_egp} EGP.")
+
+    amount_in_egp = 1000
+    amount_in_usd = converter.convert(amount_in_egp, 'EGP', 'USD')
+    print(f"{amount_in_egp} EGP is equivalent to {amount_in_usd} USD.")
